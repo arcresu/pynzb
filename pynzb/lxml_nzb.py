6d127467ac3fb2dddf85f4433d11c7642b3d4237
Carl Suster <carl@contraflo.ws>
2017-01-12 12:17:58 +1100

    Add Python 3 support to LXML parser
    The lxml etree API changed in Python 3 to take BytesIO instead of StringIO. This patch maintains the original behaviour in Python 2 but switches to BytesIO in Python 3, decoding the XML data as UTF-8.

--- pynzb/lxml_nzb.py
+++ pynzb/lxml_nzb.py
@@ -6,11 +6,17 @@ except ImportError:
     raise ImportError("You must have lxml installed before you can use the " +
         "lxml NZB parser.")
 
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
+import sys
+if sys.version_info.major < 3:
+    try:
+        from cStringIO import StringIO
+    except ImportError:
+        from StringIO import StringIO
+    def as_io(xml): return StringIO(xml)
+else:
+    from io import BytesIO
+    def as_io(xml): return BytesIO(bytes(xml, 'utf-8'))
 
 class LXMLNZBParser(BaseETreeNZBParser):
     def get_etree_iter(self, xml, et=etree):
-        return iter(et.iterparse(StringIO(xml), events=("start", "end")))
+        return iter(et.iterparse(as_io(xml), events=("start", "end")))
